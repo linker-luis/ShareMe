22308a405aeebfc2f8c6b753fb44a3dc3efe632c
linker-luis <luis.dot444@gmail.com>
2022-02-09 19:41:12 -0500

    'shareme'

---
+++ src/components/Search.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client'
+import { feedQuery, searchQuery } from '../utils/data'
+import Spinner from './Spinner';
+
+function Search({ searchTerm }) {
+  const [pins, setPins] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+  console.log(searchTerm)
+  return (
+    <div>
+
+    {loading && <Spinner message="Searching pins" />}
+    {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+    {pins?.length === 0 && searchTerm !== '' && !loading && (
+      <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+    )}
+  </div>
+  )
+}
+
+export default Search;
